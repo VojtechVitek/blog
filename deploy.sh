41ac07177e40e774defe24a42fad2c52cd4fbe15
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2015-03-24 12:45:26 -0400

    rebuilding site Tue Mar 24 12:45:26 EDT 2015

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+git push origin hugo
 git subtree push --prefix=public git@github.com:VojtechVitek/blog.git gh-pages
